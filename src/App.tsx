ffe345ab2ea698e539392f454baa20e81bb2dc1b
karenmcc0922 <karenmanuela.cardona@utp.edu.co>
2026-03-25 20:36:47 -0500

    mejoras visuales

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import ProblemSection from "./components/ProblemSection";
 import SolutionSection from "./components/SolutionSection";
 import BenefitsSection from "./components/BenefitSection";
 import CTASection from "./components/CTASection";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      {/* Navbar siempre arriba */}
+      <Navbar />
+
+      {/* Secciones principales */}
       <HeroSection />
       <ProblemSection />
       <SolutionSection />
       <BenefitsSection />
       <CTASection />
-      <Navbar/>
+
+      {/* Footer (si tienes uno) */}
+      {/* <Footer /> */}
     </div>
   );
 }
