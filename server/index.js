47a1081e7784cc4a9c6b25f1e426c615e55c7ef6
karenmcc0922 <karenmanuela.cardona@utp.edu.co>
2026-04-02 20:08:57 -0500

    Actualización

--- server/index.js
+++ server/index.js
@@ -143,6 +143,6 @@ app.post('/api/completar-perfil', (req, res) => {
 
 // --- INICIO DEL SERVIDOR ---
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
